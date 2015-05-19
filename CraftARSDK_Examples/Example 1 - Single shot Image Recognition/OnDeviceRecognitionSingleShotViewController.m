bbf0b03c049e52a42edc0bc6178802a08e2e1e43
Luis Martinell Andreu <luis@catchoom.com>
2015-05-19 10:52:42 +0200

    First commit.

---
+++ CraftARSDK_Examples/Example 1 - Single shot Image Recognition/OnDeviceRecognitionSingleShotViewController.m
@@ -0,0 +1,156 @@
+// CraftARSDK_examples is free software. You may use it under the MIT license, which is copied
+// below and available at http://opensource.org/licenses/MIT
+//
+// Copyright (c) 2015 Catchoom Technologies S.L.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of
+// this software and associated documentation files (the "Software"), to deal in
+// the Software without restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+// Software, and to permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+#import "OnDeviceRecognitionSingleShotViewController.h"
+#import <SDK_Recognition_Offline/CraftARSDK_IR.h>
+#import <SDK_Recognition_Offline/OfflineIR.h>
+
+@interface OnDeviceRecognitionSingleShotViewController () <CraftARSDKProtocol, SearchProtocol> {
+    CraftARSDK_IR *_sdk;
+    OfflineIR *_oir;
+}
+
+@end
+
+@implementation OnDeviceRecognitionSingleShotViewController
+
+#pragma mark view initialization
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+- (void) viewWillAppear:(BOOL) animated {
+    [super viewWillAppear:animated];
+    
+    // setup the CraftAR SDK
+    _sdk = [CraftARSDK_IR sharedCraftARSDK_IR];
+    
+    // Become delegate of the SDK to receive capture initialization callbacks
+    _sdk.delegate = self;
+    
+    // Initialize the video capture on our preview view.
+    [_sdk startCaptureWithView:self._preview];
+    self._previewOverlay.hidden = NO;
+    self._scanningOverlay.hidden = YES;
+}
+
+#pragma mark -
+
+
+#pragma mark Snap Photo mode implementation
+
+- (void) didStartCapture {
+    self._previewOverlay.hidden = NO;
+    
+    // Get Offline Recognition class (for on-device searches)
+    // and set it as the search controller delegate for the SDK
+    _oir = [OfflineIR sharedOfflineIR];
+    _sdk.searchControllerDelegate = _oir;
+    
+    // Set the view controller as delegate of the OfflineIR to recieve the
+    // search results
+    _oir.delegate = self;
+}
+
+- (IBAction)snapPhotoToSearch:(id)sender {
+    self._previewOverlay.hidden = YES;
+    self._scanningOverlay.hidden = NO;
+    [self._scanningOverlay setNeedsDisplay];
+    [_sdk singleShotSearch];
+    [[_sdk getCamera] stopCapture];
+    
+}
+
+- (void) didGetSearchResults:(NSArray *)resultItems {
+    self._scanningOverlay.hidden = YES;
+    
+    if ([resultItems count] >= 1) {
+        // Found one item, launch its content on a webView:
+        CraftARSearchResult *bestResult = [resultItems objectAtIndex:0];
+        CraftARItem *item = bestResult.item;
+        
+        NSString* alertText = [NSString stringWithFormat:@"Item found: '%@'", item.name];
+        if (resultItems.count > 1) {
+            alertText = [alertText stringByAppendingString: [NSString stringWithFormat:@" and %d more", resultItems.count -1]];
+        }
+        
+        UIAlertView *alert = [[UIAlertView alloc] init];
+        [alert setTitle:alertText];
+        [alert setDelegate:self];
+        [alert addButtonWithTitle:@"Ok"];
+        [alert show];
+        
+        self._previewOverlay.hidden = NO;
+        self._scanningOverlay.hidden = YES;
+        
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc] init];
+        [alert setTitle:@"Nothing found"];
+        [alert setDelegate:self];
+        [alert addButtonWithTitle:@"Ok"];
+        [alert show];
+        self._scanningOverlay.hidden = YES;
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex{
+    self._previewOverlay.hidden = NO;
+    [[_sdk getCamera] restartCapture];
+}
+
+- (void) didFailSearchWithError:(CraftARError *)error {    // Check the error type
+    NSLog(@"Error calling CRS: %@", [error localizedDescription]);
+    self._previewOverlay.hidden = NO;
+    self._scanningOverlay.hidden = YES;
+    [[_sdk getCamera] restartCapture];
+}
+
+#pragma mark -
+
+
+#pragma mark view lifecycle
+
+- (void) viewWillDisappear:(BOOL)animated {
+    [[_sdk getCamera] stopCapture];
+    [super viewWillDisappear:animated];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark -
+
+@end
---
+++ README.md
@@ -0,0 +1,46 @@
+## CraftAR - iOS Image Recognition SDK examples
+
+### Introduction
+
+The CraftAR Service for [Augmented Reality and Image Recognition](http://catchoom.com/product/craftar/augmented-reality-and-image-recognition/) is a  service
+that allows you to build a wide range of __Image Recognition__ and __Augmented Reality__ applications
+and services.
+
+With CraftAR, you can create amazing apps that provide digital content
+for real-life objects like printed media, packaging among others. You
+can use our online web panel or APIs, to upload images to be recognized and set
+content to display upon recognition in your CraftAR-powered app.
+
+The [iOS Image Recognition SDK](http://catchoom.com/documentation/sdk/image-recognition/ios-image-recognition-sdk/) focuses on opening the camera in the mobile device and performing
+Image Recognition requests to the API of CraftAR. The display of the result of the request
+of each is up to you.
+
+This document describes mainly the Examples of different uses of the Service and the Image Recognition SDK.
+General use of the SDK can be found in the Documentation webpage for the [iOS Image Recognition SDK](http://catchoom.com/documentation/sdk/image-recognition/ios-image-recognition-sdk/). Complete SDK documentation of the classes can be found within the distribution of the SDK itself.
+
+
+### How to use the examples
+
+This repository comes with an Xcode project of an iOS app with several
+examples that show how to use the SDK.
+
+To run the examples follow these steps:
+ 1.  Open the CatchoomSDK_Examples.xcodeproj project.
+ 2.  Integrate the CatchoomSDK into the Xcode project (see [below](#step-by-step-guide)).
+ 3.  Select an iOS 8 device (notice that the project will not
+     compile for the simulator).
+ 4.  Hit the run button.
+
+### Add CatchoomSDK to the Example project
+
+#### Requirements
+
+To build the project or use the library, you will need XCode 6 or newer,
+and at least the iOS 8.0 SDK.
+
+#### Step-by-step guide
+1.  Download the [CraftAR iOS Image Recognition SDK](http://catchoom.com/product/craftar/augmented-reality-and-image-recognition-sdk/#download-mobile-sdk).
+2.  Unzip the package
+3.  Drag the following files into the project:
+ * CatchoomSDK.framework
+ * Pods.framework
